79b7c9470cc66163fb30aba2814b5e21cf18b08f
MosolovIR <mosolovolosom@gmail.com>
2024-06-14 14:44:46 +0300

    readme, readcsv, showMenu fix

--- readCSV.py
+++ readCSV.py
@@ -1,12 +1,11 @@
 def read_csv(filename): 
 
     phone_book = []
-    fields = ['Фамилия', 'Имя', 'Телефон', 'Описание']
+    fields = ['Фамилия', 'Имя', 'Телефон', 'Должность']
 
     with open(filename, 'r', encoding = 'utf-8') as phb:
         for line in phb:
             record = dict(zip(fields, line.split(',')))
-		    #dict(((Фамилия, Иванов),(Имя, Иван),(Телефон, 111)))
         phone_book.append(record)	
 
     return phone_book
